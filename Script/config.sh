ccd255c646667ae3db6424693e9105d1643c76cf
diwasx <diwasx@gmail.com>
2019-03-25 18:43:49 +0545

    corrupter for lockscreen

--- Script/config.sh
+++ Script/config.sh
@@ -28,8 +28,8 @@ cp -r ~/.config/compton.conf /DriveE/Projects/Git/i3wm/.config
 mkdir /DriveE/Projects/Git/i3wm/Script
 cp -r ~/Documents/Script /DriveE/Projects/Git/i3wm
 cp -r ~/.ncmpcpp /DriveE/Projects/Git/i3wm/
-cp -r ~/.config/pulse/presets /DriveE/Projects/Git/i3wm/.config/pulse/
-cp -r ~/.config/pulse/default.pa /DriveE/Projects/Git/i3wm/.config/pulse/default.pa
+# cp -r ~/.config/pulse/presets /DriveE/Projects/Git/i3wm/.config/pulse/
+# cp -r ~/.config/pulse/default.pa /DriveE/Projects/Git/i3wm/.config/pulse/default.pa
 cp ~/.profile /DriveE/Projects/Git/i3wm/
 cp -r ~/.mplayer/ /DriveE/Projects/Git/i3wm/
 cp ~/.imwheelrc /DriveE/Projects/Git/i3wm/
