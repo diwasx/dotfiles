c165fa05c2a82bd1fc0f18d17ddeab025bc1daa2
diwasx <diwasx@gmail.com>
2019-04-14 12:55:21 +0545

    fixed conflict with floatingMpv

--- Script/i3block/mpv_i3block.sh
+++ Script/i3block/mpv_i3block.sh
@@ -1,10 +1,13 @@
 #Script that finds mpsyt (mpv) title running in background
 
-# ps aux | grep mpv | sed 's/.*title \(.*\)--no.*/\1/'
-ps aux | grep "[m]pv --title"| sed 's/.*title \(.*\)--no.*/\1/'
-pkill -RTMIN+13 i3blocks;
+# ps aux | grep "[m]pv --title"| sed 's/.*title \(.*\)--no.*/\1/'
+
+title=`ps aux | grep "[m]pv --title" | sed '/floatingMpv/d'` #Delete line that contain "floatingMpv"
+title=`echo $title | sed 's/.*title \(.*\)--no.*/\1/'`  #Extract song title
+echo $title
 
 # [m] for searching regix, because ps display echo process itself
 # \1 is a 'Remember pattern' that remembers everything that is within \(.*\) 
 # .*xyz --> represent starting of string
 
+pkill -RTMIN+13 i3blocks;   #Signal to i3block to update song
--- Script/mpsytWatch.sh
+++ Script/mpsytWatch.sh
@@ -3,6 +3,6 @@
 while true; do
     title=`$HOME/Documents/Script/i3block/mpv_i3block.sh`
     notify-send "$title"
-	pkill -RTMIN+13 i3blocks
+	# pkill -RTMIN+13 i3blocks
     watch -n 3 -g $HOME/Documents/Script/i3block/mpv_i3block.sh
 done
