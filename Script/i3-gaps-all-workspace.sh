e72a029ac8733c14b0ace68a99b246c6717980f8
diwasx <diwasx@gmail.com>
2019-05-25 21:23:33 +0545

    conky-i3

--- Script/i3-gaps-all-workspace.sh
+++ Script/i3-gaps-all-workspace.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-#Script replacing "gaps all" fixing i3-reload to resetting buffer i3 config
-# bindsym mod1+Shift+grave gaps bottom all set 70
+# Difference i3-gap scheme binded to buttons by passing argument
 
 # Current workspace
 current_ws=`i3-msg -t get_workspaces \
   | jq '.[] | select(.focused==true).name' \
   | cut -d"\"" -f2`
-echo $current_ws
+# echo $current_ws
 
 workspace1="1. Terminal" 
 workspace2="2. Web"
@@ -20,19 +19,40 @@ workspace8="8"
 workspace9="9."
 workspace10="10. Music"
 
-# Setting value to each workspace
-i3-msg "workspace $workspace1; gaps bottom current set 70"
-i3-msg "workspace $workspace2; gaps bottom current set 70"
-i3-msg "workspace $workspace3; gaps bottom current set 70"
-i3-msg "workspace $workspace4; gaps bottom current set 70"
-i3-msg "workspace $workspace5; gaps bottom current set 70"
-i3-msg "workspace $workspace6; gaps bottom current set 70"
-i3-msg "workspace $workspace7; gaps bottom current set 70"
-i3-msg "workspace $workspace8; gaps bottom current set 70"
-i3-msg "workspace $workspace9; gaps bottom current set 70"
-i3-msg "workspace $workspace10; gaps bottom current set 70"
+set_gap_all_workspace(){
+    # This function replaces "gaps all" fixing i3-reload that was resetting buffer i3 config.
+    # bindsym mod1+Shift+grave gaps bottom all set 70
 
-# Return to current ws
-i3-msg "workspace $current_ws"
+    # Setting value to each workspace
+    i3-msg "workspace $workspace1; $1"
+    i3-msg "workspace $workspace2; $1"
+    i3-msg "workspace $workspace3; $1"
+    i3-msg "workspace $workspace4; $1"
+    i3-msg "workspace $workspace5; $1"
+    i3-msg "workspace $workspace6; $1"
+    i3-msg "workspace $workspace7; $1"
+    i3-msg "workspace $workspace8; $1"
+    i3-msg "workspace $workspace9; $1"
+    i3-msg "workspace $workspace10; $1"
 
-# Problem: Works only for active workspace
+    # Return to current ws
+    i3-msg "workspace $current_ws"
+
+    # Problem: Works only for active workspace
+}
+
+if [[ $1 == "vis" ]]; then
+    set_gap_all_workspace "gaps bottom current set 70"
+elif [[ $1 == "vis-reset" ]]; then
+    set_gap_all_workspace "gaps bottom current set 0"
+elif [[ $1 == "conky" ]]; then
+    set_gap_all_workspace "gaps left current set 234; gaps right current set 320"
+elif [[ $1 == "conky-reset" ]]; then
+    set_gap_all_workspace "gaps left current set 0; gaps right current set 0"
+elif [[ $1 == "terminal-gap" ]]; then
+    i3-msg "gaps inner current set 10"
+elif [[ $1 == "reset" ]]; then
+    i3-msg "gaps inner all set 0; gaps outer all set 0"
+else
+    echo "Use argument vis, conky, terminal-gap or reset"
+fi
--- Script/vis_i3.sh
+++ Script/vis_i3.sh
@@ -5,11 +5,9 @@ ps aux | grep "[v]is floating" > /dev/null
 if [ $? == 0 ]; then
     echo "Running"
     pkill -f "vis floating"
-    i3-msg "gaps inner all set 0; gaps outer all set 0"
+    # i3-msg "gaps inner all set 0; gaps outer all set 0"
+    i3-gaps-all-workspace.sh vis-reset
 else
     echo "Not Running"
-    i3-gaps-all-workspace.sh && exec urxvt -name "vis floating" -fn "xft:Bitstream Vera Sans Mono:pixelsize=7" -e vis 
+    i3-gaps-all-workspace.sh vis && exec urxvt -name "vis floating" -fn "xft:Bitstream Vera Sans Mono:pixelsize=7" -e vis 
 fi
-
-
-
