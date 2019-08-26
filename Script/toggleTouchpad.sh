c148e13472ff1a56d8dad2ab53ae14228de89b97
diwasx <diwasx@gmail.com>
2019-08-26 11:47:02 +0545

    touchpad button fixed

--- Script/toggleTouchpad.sh
+++ Script/toggleTouchpad.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 declare -i ID
 ID=`xinput list | grep -Eio '(touchpad|glidepoint)\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}'`
-if lsusb --verbose 2>&1 | grep --quiet "Mouse"
+declare -i STATE
+STATE=`xinput list-props $ID|grep 'Device Enabled'|awk '{print $4}'`
+if [ $STATE -eq 1 ]
 then
+    pkill -f "syndaemon"
+
     xinput disable $ID
     echo "Touchpad disabled."
     notify-send -a 'Touchpad' 'Disabled' -i /home/lanaya/Documents/icons/icons8-touchpadOff-50.png
