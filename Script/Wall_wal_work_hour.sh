4f8b26e6f84d232c6c5d84206752f99876bae1b3
diwasx <diwasx@gmail.com>
2019-08-29 09:18:14 +0545

    wal_work

---
+++ Script/Wall_wal_work_hour.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+pkill -f "Wall_wal.sh"
+# If process already running
+if pidof -o %PPID -x "Wall_wal_work_hour.sh">/dev/null; then
+    folder="/DriveE/Pictures/Wallpapers/Collection/work_hour/"
+    file=`find $folder |shuf -n1`
+
+    #Transition Effect
+    convert $file -fill black -colorize 50% /tmp/wal1.jpg
+    convert /tmp/wal1.jpg -fill black -colorize 50% /tmp/wal2.jpg
+    feh --bg-fill /tmp/wal2.jpg
+    feh --bg-fill /tmp/wal1.jpg
+
+    wal -i $file; /home/lanaya/Documents/Script/reloadDunst.sh
+    echo "Process already running"
+else
+    # If process not running
+    while true; do
+        folder="/DriveE/Pictures/Wallpapers/Collection/work_hour/"
+        file=`find $folder |shuf -n1`   #'find' generates full path and 'shuf' shuffles
+
+        #Transition Effect
+        convert $file -fill black -colorize 50% /tmp/wal1.jpg
+        convert /tmp/wal1.jpg -fill black -colorize 50% /tmp/wal2.jpg
+        feh --bg-fill /tmp/wal2.jpg
+        feh --bg-fill /tmp/wal1.jpg
+
+        wal -i $file; /home/lanaya/Documents/Script/reloadDunst.sh
+        # sleep 3600
+        sleep 1800
+    done
+fi
